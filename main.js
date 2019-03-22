ce21c693a91ea9a6ed789e11c0729872f8b39f87
Prian Vladislav <32475886+VladPrian@users.noreply.github.com>
2019-03-22 11:32:19 +0200

    first

---
+++ main.js
@@ -0,0 +1,15 @@
+function calc() {
+	        let num1 = Number(document.querySelector("#num1").value);
+	        let num2 = Number(document.querySelector("#num2").value);
+	        let sum = num1 + num2;
+	        document.getElementById("sum").value = sum;
+	    }
+
+function replace() {
+	        let num11 = Number(document.querySelector("#num11").value);
+	        let replace = Number(document.querySelector("#replace").value);
+	        let chace = sum;
+	        let sum = num11;
+	        document.getElementById("replace").value = sum;
+	        document.getElementById("num11").value = chace;
+	    }
